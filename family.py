c345536708b442565a1257d58c276b201f08e7c1
hkl-iii <rahleggon@gmail.com>
2021-04-27 13:50:58 -0500

    "completed question 2 of assignment. Created family file for it"

---
+++ family.py
@@ -0,0 +1,31 @@
+family = [{
+    'first_name': 'luisa',
+    'last_name': 'uribe-leggon',
+    'relationship': 'wife'
+},
+    {
+        'first_name': 'diana',
+        'last_name': 'leggon',
+        'relationship': 'daughter'
+    },
+    {
+        'first_name': 'daniela',
+        'last_name': 'uribe',
+        'relationship': 'step-daughter'
+    },
+{
+        'first_name': 'sofia',
+        'last_name': 'uribe',
+        'relationship': 'step-daughter'
+    },
+{
+        'first_name': 'chanel',
+        'last_name': 'leggon',
+        'relationship': 'puppy'
+    },
+{
+        'first_name': 'emiliano',
+        'last_name': 'uribe',
+        'relationship': 'dog'
+    }
+]
